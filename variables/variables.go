1ac7203eb1e8d916f050509db73ab53adf271a3c
Best Verie Iradukunda <60257249+Best-Verie@users.noreply.github.com>
2021-08-15 19:10:02 +0200

    short variable declaration

--- variables/variables.go
+++ variables/variables.go
@@ -5,4 +5,14 @@ var a, isBoy, isGirl bool = true, false, true;
 func main(){
 	var i int = 10;
 	fmt.Println(i,a,isBoy,isGirl);
+
+	// short variable declarations
+
+	k := 100;
+	c,python,java := true,false,"stronger!";
+
+	fmt.Println(k,c,python,java);
+
+
+
 }
